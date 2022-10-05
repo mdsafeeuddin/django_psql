e073fb4274c144d089043925bdbecc7782c6594f
mdsafeeuddin <mdsafeeuddin@gmail.com>
2022-10-05 23:45:57 +0530

    data upload with models.

--- sql/admin.py
+++ sql/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Section
+from .models import Topic
+
+admin.site.register(Section)
+admin.site.register(Topic)
--- sql/models.py
+++ sql/models.py
@@ -1,3 +1,99 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Department(models.Model):
+  department = models.CharField(max_length=200, primary_key=True)
+  division = models.CharField(max_length=200)
+
+  def __str__(self) -> str:
+    return self.department, self.division
+
+  class Meta:
+    db_table = 'departments'
+
+class Region(models.Model):
+  region_id = models.IntegerField(primary_key=True)
+  region = models.CharField(max_length=200)
+  country = models.CharField(max_length=200)
+
+  class Meta:
+    db_table = 'regions'
+
+class Employee(models.Model):
+  employee_id = models.IntegerField(primary_key=True)
+  first_name = models.CharField(max_length=200)
+  last_name = models.CharField(max_length=200)
+  email = models.CharField(max_length=200, null=True)
+  hire_date = models.DateField()
+  department = models.CharField(max_length=200)
+  gender = models.CharField(max_length=200)
+  salary = models.IntegerField()
+  region_id = models.IntegerField()
+
+  class Meta:
+    db_table = 'employees'
+
+class Student(models.Model):
+  student_no = models.IntegerField(primary_key=True)
+  student_name = models.CharField(max_length=200)
+  age = models.IntegerField()
+
+  class Meta:
+    db_table = 'students'
+
+class Course(models.Model):
+  course_no = models.CharField(max_length=200, primary_key = True)
+  course_title = models.CharField(max_length=200)
+  credits = models.IntegerField()
+
+  class Meta:
+    db_table = 'courses'
+
+class StudentEnrollment(models.Model):
+  student_no = models.IntegerField(primary_key = True, unique=False)
+  course_no = models.CharField(max_length=200)
+
+  class Meta:
+    db_table = 'student_enrollment'
+
+class Professor(models.Model):
+  last_name = models.CharField(max_length=200, primary_key=True)
+  department = models.CharField(max_length=200)
+  salary = models.IntegerField()
+  hire_date = models.DateField('hire_date')
+
+  class Meta:
+    db_table = 'professors'
+
+class Teach(models.Model):
+  last_name = models.CharField(max_length=200, primary_key=True, unique=False)
+  course_no = models.CharField(max_length=200)
+
+  class Meta:
+    db_table = 'teach'
+
+class Section(models.Model):
+  section_title = models.CharField(max_length=200)
+  created = models.DateTimeField('date created')
+  updated = models.DateTimeField('date updated')
+
+  def __str__(self) -> str:
+    return self.section_title
+
+  class Meta:
+    db_table = 'sections'
+  
+class Topic(models.Model):
+  section = models.ForeignKey(Section, on_delete = models.CASCADE)
+  topic = models.CharField(max_length=200)
+  duration = models.IntegerField(default=0)
+  created = models.DateTimeField('date created')
+  updated = models.DateTimeField('date updated')
+
+  def __str__(self) -> str:
+    return self.topic
+
+  class Meta:
+    db_table = 'topics'
